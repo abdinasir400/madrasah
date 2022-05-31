acea4bbd8a5dea1e7d87197ac9b102156e980212
Abdinasir Mohamed <abdinasir400@gmail.com>
2022-05-31 23:35:26 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
 
-console.log("Hello world, Abdinasir")
+const app = express()
+
+app.use('/api/students',require('./Routes/studentsroutes'))
+
+app.listen(port,() => console.log(`Server started on port ${port}`))
