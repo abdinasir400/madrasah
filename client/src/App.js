d52d7510bfc9ef62d65de5556199ae722570ea76
Abdinasir Mohamed <abdinasir400@gmail.com>
2022-06-15 21:59:26 +0100

    New code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Dashboard from './routes/Dashboard';
+import Login from './routes/Login';
+import Register from './routes/Register';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Madrasa app</h1>
-    </div>
+    <>
+    <Navbar/>
+    <Router>
+      <div className = 'container'>
+        <Routes>
+          <Route path = '/' element = {<Dashboard />} />
+          <Route path = '/login' element = {<Login />} />
+          <Route path = '/register' element = {<Register />} />
+
+        </Routes>
+
+      
+      </div>
+      </Router>
+    
+    
+
+    </>
   );
 }
 
--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -1,43 +1,15 @@
-const Navbar = () => {
-    return (
-      <div>
-        <>
-    <Navbar bg="light" variant="dark">
-      <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
-    <br />
-    <Navbar bg="primary" variant="dark">
-      <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
-  
-    <br />
-    <Navbar bg="light" variant="light">
-      <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
-  </>
-      </div>
-    )
-  }
-  
-  export default Navbar
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux' 
+import React from 'react'
+
+
+function Dashboard() {
+    return <div>  Dashboard</div>
+    
+
+}
+
+export default Dashboard
+
+
