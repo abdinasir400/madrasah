34e3da22ecf4d142b02a5ca48828d8123d9090df
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-06-17 19:25:11 +0500

    Updating data

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navbar from "./components/Navbar";
+import RecordList from "./components/database/recordList";
+import Edit from "./components/database/edit";
+import Create from "./components/database/create";
 
 // Global CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
