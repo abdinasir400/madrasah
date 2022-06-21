1e415fead6313dec2eb1ab6ea3d4abad4cee8e1d
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-06-22 04:03:17 +0500

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,11 @@
 import React from "react";
-
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
-
-// We import all the components we need in our app
-import Navbar from "./components/Navbar";
-import RecordList from "./components/database/recordList";
-import Edit from "./components/database/edit";
-import Create from "./components/database/create";
-
-// Global CSS
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<RecordList />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-    </div>
-  );
-};
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
